932d9e4e58ecbe1c17662f27a24263cb322d490d
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-02-10 08:53:20 -0800

    2023-02-10, 0853

--- C2/Client.cs
+++ C2/Client.cs
@@ -1,11 +1,24 @@
 
 using System;
-using System.Runtime.InteropServices;
+using System.Net;
+using System.Net.Sockets;
 
-namespace Server {
+namespace Client {
   class Program {
-    static void Main(string[] args) {
+    IPAddress ip = IPAddress.Parse("127.0.0.1");
+    IPEndPoint server = new IPEndPoint(ip, 4444);
+    
+    Socket client = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+    int BUFFER_SIZE = 2048;
+    byte[] bytes = new byte[BUFFER_SIZE];
 
+    static void Main(string[] args) {
+      client.Connect(server);
+      Array.Clear(bytes, 0, bytes.Length); // clear the buffer, from the first to last byte
+      client.Receive(bytes);
+      message = Encoding.ASCII.GetString(bytes).TrimEnd('\0');
+      Console.WriteLine("{0}", message);
+      client.Close();
     }
   }
 }
--- C2/Server.cs
+++ C2/Server.cs
@@ -1,11 +1,23 @@
 
 using System;
-using System.Runtime.InteropServices;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Server {
   class Program {
+    Socket server = new Socket(IPAddress.Any.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+    IPEndPoint address = new IPEndPoint(IPAddress.Any, 4444);
+    string message = "OK";
+    
     static void Main(string[] args) {
+      server.Bind(address);
+      server.Listen(5);
 
+      Socket client = server.Accept();
+      client.send(Encoding.ASCII.GetBytes(message));
+      
+      client.close();
+      server.close();
     }
   }
 }
