5de86044613c7d71f0aa1601d2e78b197d5bc90c
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-02-15 20:13:30 -0800

    Update Processes.cs

--- EDR/Processes.cs
+++ EDR/Processes.cs
@@ -7,42 +7,31 @@ using System.Runtime.InteropServices; // provides DllImport
 using System.Text; // provides Encoding
 
 namespace RedShed {
-    class ProcessInfo {
-        public int Id;
-        public string Name;
-        public IntPtr Handle;
-        public int ModuleCount;
-    }
-
-    class Processes {
+    class Processes {      
         [DllImport("kernel32.dll")]
-        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, ref IntPtr lpNumberOfBytesRead);
 
-        static ProcessInfo GetProcessInfo(Process p) {
-            ProcessInfo pinfo = new ProcessInfo();
-            pinfo.Id = p.Id;
-            pinfo.Name = p.ProcessName;
-            pinfo.Handle = p.Handle;
-            pinfo.ModuleCount = p.Modules.Count;
-            return pinfo;
+        static void PrintBytes(byte[] bytes) {
+            var sb = new StringBuilder();
+            foreach (var b in bytes) {
+                sb.Append(b);
+            }
+            Console.WriteLine(sb.ToString());
         }
 
-        static void GetProcessModuleMemory(Process p, ProcessModule m) {
-            int bytesRead = 0;
-            byte[] buffer = new byte[m.ModuleMemorySize];
-            ReadProcessMemory((int) p.Handle, (int) m.BaseAddress, buffer, buffer.Length, ref bytesRead);
+        static void GetProcessMemory(Process p) {
+            IntPtr bytesRead = IntPtr.Zero;
+            byte[] buffer = new byte[p.MainModule.ModuleMemorySize];
+            ReadProcessMemory(p.Handle, p.MainModule.BaseAddress, buffer, buffer.Length, ref bytesRead);
             Console.WriteLine(Encoding.Unicode.GetString(buffer));
+            // PrintBytes(buffer);
         }
 
         static void Main() { 
             foreach (Process p in Process.GetProcesses()) {
                 try {
-                    if (p.ProcessName == "notepad") {
-                        Console.WriteLine(GetProcessInfo(p.GetType().GetProperties()));
-                        foreach (ProcessModule m in p.Modules) {
-                            //Console.WriteLine(m);
-                            //GetProcessModuleMemory(p, m);
-                        }
+                    if (p.ProcessName == "notepad") { 
+                        GetProcessMemory(p);
                     }
                 } catch (Exception e) {
                     Console.WriteLine(e.Message);
@@ -54,12 +43,12 @@ namespace RedShed {
 }
 
 /* 
+    ReadProcessMemory example
     https://codingvision.net/c-read-write-another-process-memory
-    var sb = new StringBuilder();
-    foreach (var b in buffer) {
-        sb.Append(b + " ");
-    }
-    Console.WriteLine(sb.ToString());
+
+    Print Byte Array example
     https://gist.github.com/aal89/150c2af2f9dfd38dafaba4ac55317366
+
+    Windows API - ProcessModule
     https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.processmodule?view=net-7.0
 */
